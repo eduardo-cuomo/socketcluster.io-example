1f3a1a2ed3e10cd8b6a06b51d39a66f7b53e0a0d
Eduardo Cuomo <reduardo7@gmail.com>
2018-12-03 11:02:42 -0300

    project setup

--- setup.sh
+++ setup.sh
@@ -6,15 +6,8 @@
     nvm install
   fi
 
-  _clone() {
-    local git="$1"
-    local dir="$2"
-
-    if [ ! -d "${dir}" ]; then
-      git clone ${git} "${dir}"
-    fi
-
-    ( cd "${dir}"
+  _install() {
+    ( cd "${1}"
       if type yarn >/dev/null 2>&1 ; then
         yarn install
       else
@@ -23,7 +16,20 @@
     )
   }
 
+  _clone() {
+    local git="$1"
+    local dir="$2"
+
+    if [ ! -d "${dir}" ]; then
+      git clone ${git} "${dir}"
+    fi
+
+    _install "${dir}"
+  }
+
   _clone https://github.com/SocketCluster/scc-state      scc-state
   _clone https://github.com/SocketCluster/scc-broker     scc-broker
   _clone https://github.com/rapidops/sc-redis-broker.git sc-redis-broker
+
+  _install server
 )
